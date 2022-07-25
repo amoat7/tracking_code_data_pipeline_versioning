2859bda297e8fb357609345629c27133ab6095c9
dave <amoat7@gmail.com>
2022-07-25 17:41:24 +1000

    commit

---
+++ tracking_code.py
@@ -0,0 +1,48 @@
+
+# %%
+import mlflow
+import os
+
+# set environment variables to datbricks uri
+os.environ['MLFLOW_TRACKING_URI'] = 'databricks'
+os.environ['DATABRICKS_HOST']='https://3167582856151142.2.gcp.databricks.com/'
+os.environ['DATABRICKS_TOKEN']='dapid40dd00647d12272515de832ee3793d8'
+EXPERIMENT_NAME = '/Users/d.amoateng110@gmail.com/deep_learing_experiment1'
+
+mlflow.set_experiment(EXPERIMENT_NAME)
+experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)
+print("experiment_id:", experiment.experiment_id)
+
+# %%
+# enable autologgin
+
+mlflow.tensorflow.autolog()
+with mlflow.start_run(experiment_id=experiment.experiment_id, run_name='keras_model'):
+    # first neural network with keras tutorial
+    from numpy import loadtxt
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import Dense
+
+    # load the dataset
+    dataset = loadtxt('pima-indians.txt', delimiter=',')
+    # split into input (X) and output (y) variables
+    X = dataset[:,0:8]
+    y = dataset[:,8]
+
+    # define the keras model
+    model = Sequential()
+    model.add(Dense(12, input_shape=(8,), activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+
+
+    # compile the keras model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # fit the keras model on the dataset
+    model.fit(X, y, epochs=150, batch_size=10)
+
+    # evaluate the keras model
+    _, accuracy = model.evaluate(X, y)
+    print('Accuracy: %.2f' % (accuracy*100))
+# %%

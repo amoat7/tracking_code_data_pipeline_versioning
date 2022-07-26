5be14f445adef6429ae2027a7f752247ad196610
dave <amoat7@gmail.com>
2022-07-26 04:40:14 +0000

    commit

--- pipeline_tracking.py
+++ pipeline_tracking.py
@@ -9,3 +9,5 @@ os.environ['MLFLOW_TRACKING_URI'] = 'http://mlflow_nginx:80'
 os.environ["AWS_ACCESS_KEY_ID"] = "minio"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
 os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://minio:9000"
+
+# download >> run_model >> regiater_model

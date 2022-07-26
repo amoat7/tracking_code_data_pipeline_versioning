0ddbdd6e7b3dcccfdd35f27195d7f8e627ed6c22
dave <amoat7@gmail.com>
2022-07-26 23:45:57 +0000

    download_data

--- pipeline/download_data.py
+++ pipeline/download_data.py
@@ -3,6 +3,7 @@ import mlflow
 import os
 import logging
 import click
+import requests
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -14,7 +15,13 @@ logger = logging.getLogger()
 def task(download_url, local_folder, pipeline_run_name):
     with mlflow.start_run(run_name=pipeline_run_name) as mlrun:
         logger.info(f"Downloading data from {download_url}")
+        r = requests.get(download_url, allow_redirects=True)
+        open('data.csv', 'wb').write(r.content)
         mlflow.log_param("download_url", download_url)
+        mlflow.log_param("local_folder", local_folder)
+        mlflow.log_param("mlflow run id", mlflow.info.run_id)
+        mlflow.set_tag('pipeline_step', __file__)
+        mlflow.log_artifacts(local_folder, artifact_path="data")
 
     logger.info(f"Finished downloading data to {local_folder}")
 

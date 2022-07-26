49c3bbd4c0fc05951a804f63cabcd9004c747e9a
dave <amoat7@gmail.com>
2022-07-26 00:45:18 +0000

    pipeline_tracking

---
+++ pipeline_tracking.py
@@ -0,0 +1,11 @@
+import mlflow
+import os
+
+#docker network connect bridge mlflow_nginx
+#docker network connect mlflow_docker_setup_frontend awesome_gates
+#docker network connect mlflow_docker_setup_storage awesome_gates
+# set environment variables to datbricks uri
+os.environ['MLFLOW_TRACKING_URI'] = 'http://mlflow_nginx:80'
+os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://minio:9000"
--- tracking_code.py
+++ tracking_code.py
@@ -3,11 +3,15 @@
 import mlflow
 import os
 
+#docker network connect bridge mlflow_nginx
+#docker network connect mlflow_docker_setup_frontend awesome_gates
+#docker network connect mlflow_docker_setup_storage awesome_gates
 # set environment variables to datbricks uri
-os.environ['MLFLOW_TRACKING_URI'] = 'databricks'
-os.environ['DATABRICKS_HOST']='https://3167582856151142.2.gcp.databricks.com/'
-os.environ['DATABRICKS_TOKEN']='dapid40dd00647d12272515de832ee3793d8'
-EXPERIMENT_NAME = '/Users/d.amoateng110@gmail.com/deep_learing_experiment1'
+os.environ['MLFLOW_TRACKING_URI'] = 'http://mlflow_nginx:80'
+os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://minio:9000"
+EXPERIMENT_NAME = 'code_tracking'
 
 mlflow.set_experiment(EXPERIMENT_NAME)
 experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)

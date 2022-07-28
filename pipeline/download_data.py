654ec600c9821decc320679492ee12f2496444ab
dave <amoat7@gmail.com>
2022-07-28 03:32:04 +0000

    add

--- pipeline/download_data.py
+++ pipeline/download_data.py
@@ -10,7 +10,7 @@ logger = logging.getLogger()
 
 @click.command(help="This program downloads data for training deep learning model")
 @click.option("--download_url", default="https://raw.githubusercontent.com/amoat7/GCP-APIs/master/dataset.csv", help="remote url for downloading training data")
-@click.option("--local_folder", default="data", help="This is a local data folder")
+@click.option("--local_folder", default="./data", help="This is a local data folder")
 @click.option("--pipeline_run_name", default="pipeline", help="This is the mlflow run name")
 def task(download_url, local_folder, pipeline_run_name):
     with mlflow.start_run(run_name=pipeline_run_name) as mlrun:

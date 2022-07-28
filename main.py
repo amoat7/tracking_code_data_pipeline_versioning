0fe2be05ef6b271740071f028e9d375619f6bd16
dave <amoat7@gmail.com>
2022-07-28 06:53:51 +0000

    add

--- main.py
+++ main.py
@@ -33,21 +33,21 @@ def run_pipeline(steps):
 
     with mlflow.start_run(run_name='pipeline', nested=True) as active_run:
         if "download_data" in active_steps:
-            download_run = mlflow.run(".", "download_data", parameters={})
+            download_run = mlflow.run("/com.docker.devenvironments.code/tracking_code_data_pipeline_versioning/", "download_data", parameters={})
             download_run = mlflow.tracking.MlflowClient().get_run(download_run.run_id)
             file_path_uri = download_run.data.params['local_folder']
             logger.info(f'downloaded data is located locally in folder: {file_path_uri}')
             logger.info(download_run)
 
         if "train_model" in active_steps:
-            training_model_run = mlflow.run("", "train_model", parameters={"data_path": file_path_uri})
+            training_model_run = mlflow.run("/com.docker.devenvironments.code/tracking_code_data_pipeline_versioning/", "train_model", parameters={"data_path": file_path_uri})
             training_model_run_id =  training_model_run.run_id
             training_model_run = mlflow.tracking.MlflowClient().get_run(training_model_run.run_id)
             logger.info(training_model_run)
 
         if "register_model" in active_steps:
             if training_model_run_id is not None and training_model_run_id!= 'None':
-                register_model_run = mlflow.run(".", "register_model", parameters={"mlflow_run_id": training_model_run_id})
+                register_model_run = mlflow.run("/com.docker.devenvironments.code/tracking_code_data_pipeline_versioning/", "register_model", parameters={"mlflow_run_id": training_model_run_id})
                 register_model_run = mlflow.tracking.MlflowClient().get_run(register_model_run.run_id)
                 logger.info(register_model_run)
             else:

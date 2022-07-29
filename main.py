1f6e5e52da985e92ca3c7b9dd5feacdb3cc8ce2b
dave <amoat7@gmail.com>
2022-07-29 00:51:34 +0000

    change requirements.txt

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import click
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
+EXPERIMENT_NAME = 'dl_model_pipeline'
+
+
+mlflow.set_experiment(EXPERIMENT_NAME)
 
 _steps = [
     "download_data",
@@ -15,10 +19,10 @@ _steps = [
 @click.command()
 @click.option("--steps", default="all", type=str)
 def run_pipeline(steps):
-    os.environ['MLFLOW_TRACKING_URI'] = 'http://mlflow_nginx:80'
-    os.environ["AWS_ACCESS_KEY_ID"] = "minio"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
-    os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://minio:9000"
+    #os.environ['MLFLOW_TRACKING_URI'] = 'http://mlflow_nginx:80'
+    #os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+    #os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
+    #os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://minio:9000"
 
     EXPERIMENT_NAME = 'dl_model_pipeline'
     mlflow.set_experiment(EXPERIMENT_NAME)

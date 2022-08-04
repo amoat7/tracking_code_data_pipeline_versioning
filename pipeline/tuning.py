3fa9c2f82509c045f36ae72077354db4a4c7db3b
David <amoat7@gmail.com>
2022-08-04 05:06:24 +0000

    add hpo

--- pipeline/tuning.py
+++ pipeline/tuning.py
@@ -8,7 +8,7 @@ import pandas as pd
 import os
 import keras_tuner
 
-EXPERIMENT_NAME = '/Users/d.amoateng110@gmail.com/dl_model_pipeline'
+EXPERIMENT_NAME = '/Users/d.amoateng110@gmail.com/dl_model_pipeline_tuning'
 mlflow.set_experiment(EXPERIMENT_NAME)
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -28,29 +28,29 @@ def build_model(hp):
 
 def task(pipeline_run_name="pipeline"):
     mlflow.tensorflow.autolog()
-    #with mlflow.start_run(run_name=pipeline_run_name) as mlrun:
-    # first neural network with keras tutorial
-    # load the dataset
-    dataset = pd.read_csv("https://raw.githubusercontent.com/amoat7/GCP-APIs/master/dataset.csv")
-    # split into input (X) and output (y) variables
-    X = dataset.iloc[:,0:8]
-    y = dataset.iloc[:,8]
+    with mlflow.start_run(run_name=pipeline_run_name) as mlrun:
+        # first neural network with keras tutorial
+        # load the dataset
+        dataset = pd.read_csv("https://raw.githubusercontent.com/amoat7/GCP-APIs/master/dataset.csv")
+        # split into input (X) and output (y) variables
+        X = dataset.iloc[:,0:8]
+        y = dataset.iloc[:,8]
 
-    
-    tuner = keras_tuner.RandomSearch(
-        build_model,
-        objective='val_loss',
-        max_trials=5)
-    # fit the keras model on the dataset
-    tuner.search(X, y, epochs=5, batch_size=10, validation_split=0.1)
+       
+        tuner = keras_tuner.RandomSearch(
+            build_model,
+            objective='val_loss',
+            max_trials=50)
+        # fit the keras model on the dataset
+        tuner.search(X, y, epochs=3, batch_size=10, validation_split=0.1)
 
-    best_model = tuner.get_best_models()[0]
+        best_model = tuner.get_best_models()[0]
 
-    #run_id = mlrun.info.run_id
-    #logger.info("run_id: {}; lifecycle_stage: {}".format(run_id,
-                                                            #mlflow.get_run(run_id).info.lifecycle_stage))
-    #mlflow.log_param("fine_tuning_mlflow_run_id", run_id)
-    mlflow.set_tag('pipeline_step', __file__)
+        run_id = mlrun.info.run_id
+        logger.info("run_id: {}; lifecycle_stage: {}".format(run_id,
+                                                             mlflow.get_run(run_id).info.lifecycle_stage))
+        mlflow.log_param("fine_tuning_mlflow_run_id", run_id)
+        mlflow.set_tag('pipeline_step', __file__)
 
 
 
